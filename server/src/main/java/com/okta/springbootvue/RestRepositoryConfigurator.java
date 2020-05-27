8b7fb34a029b75ca96578bcd7c2323f6eee6af3a
Daniel Clarke <deliotclarke@gmail.com>
2020-05-27 11:11:28 -0500

    initial setup for server completed. initial creation of Vue application completed.

---
+++ server/src/main/java/com/okta/springbootvue/RestRepositoryConfigurator.java
@@ -0,0 +1,18 @@
+package com.okta.springbootvue;
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * IDs are not returned by the RestRepository by default. They are exposed so that
+ * the client can easily find the ID of created and listed resources.
+ */
+@Component
+public class RestRepositoryConfigurator extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Todo.class);
+    }
+}
