a0fb2d39757e9d560c64b8aaf5c139a3bfd4572b
Daniel Clarke <deliotclarke@gmail.com>
2020-05-28 11:42:54 -0500

    added okta package and vue router.

---
+++ client/src/router.js
@@ -0,0 +1,35 @@
+import Auth from '@okta/okta-vue';
+import Vue from 'vue';
+import Router from 'vue-router';
+import Todos from './components/Todos';
+
+Vue.use(Auth, {
+  issuer: 'https://dev-610938-admin.okta.com/oauth2/default',
+  client_id: '0oada2kbxSzAv2btE4x6',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  scope: 'openid profile email',
+});
+
+Vue.use(Router);
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Todos',
+      component: Todos,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/implicit/callback',
+      component: Auth.handleCallback(),
+    },
+  ],
+});
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard());
+
+export default router;
